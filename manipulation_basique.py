9adb61a50e0107ea1b895ac9421db4ad420a6e4a
vwez <desirv63@gmail.com>
2020-10-20 02:15:36 +0200

    premier commit du TP

---
+++ manipulation_basique.py
@@ -0,0 +1,38 @@
+# prmier tp
+chaine = "chaine de caracteres"
+# print(chaine)
+# type(chaine)
+nb = 15
+# print(nb)
+# type(nb)
+tab = [12,'un','vingt-2',0]
+# print(tab)
+# type(tab)
+dico = {'un':'lundi', 'deux':'mardi', 'trois' :'mercredi'}
+# print(dico)
+# type(dico)
+nb_compo = 12.5 
+# print(nb_compo)
+# type(nb_compo)
+boolean = True
+# print(boolean)
+# type(boolean)
+boolean_2 = False
+# print(boolean_2)
+# type(boolean_2)
+tuple_a = (1,'un')
+# print(tuple_a)
+# type(tuple_a)
+
+# deuxieme partie du tp
+# affecter un type à une variable
+a = tuple()
+print(a)
+type(a)
+
+'''
+Réponse à la Question
+Lorsque l’on utilise cette méthode, la valeur initiale de la variable est 0, quand il s'agit d'un entier 
+pour ce qui est des autres types de données, ces variables sont vides
+'''
+
